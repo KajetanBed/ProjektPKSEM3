b4acf872e67dd5be8427f4e68021060234dc2120
KajetanBed <kajetanmbednarczyk@gmail.com>
2025-11-18 19:26:25 +0100

    Urozmaicone Anyżowe Rwanko

---
+++ UAR.cpp
@@ -0,0 +1,10 @@
+#include "UAR.h"
+
+double UAR::symuluj(double zadanie)
+{
+    double e = zadanie - y_prev;
+    double u = pid.oblicz(e);
+    double y = model.symuluj(u);
+    y_prev = y;
+    return y;
+}
