ef6bae4ead7e853652a77eaf80e1b12c80991e62
KajetanBed <kajetanmbednarczyk@gmail.com>
2026-01-15 00:18:22 +0100

    poprawa

--- ustawieniaarx.h
+++ ustawieniaarx.h
@@ -17,12 +17,11 @@ public:
     explicit UstawieniaARX(QWidget *parent = nullptr);
     ~UstawieniaARX();
 
-    // Metody do ustawiania pól w oknie (pre-fill)
+    // Metody do ustawiania pól w oknie 
     void setWielomianA(const std::vector<double> &a);
     void setWielomianB(const std::vector<double> &b);
     void setK(int k);
     void setSzum(double stdev);
-    // ... analogicznie settery dla limitów jeśli chcesz
 
     // Metody do pobierania danych po zatwierdzeniu
     std::vector<double> getA() const;
@@ -41,7 +40,6 @@ private slots:
     void on_btnZatwierdz_clicked();
     void on_btnAnuluj_clicked();
 
-private:
 private:
     Ui::UstawieniaARX *ui;
     std::vector<double> parsujWektor(QString tekst) const;
